64932df96507165182a861ef817864de605cc669
Marlon Martins <marlon673@hotmail.com>
2022-05-03 15:20:37 -0300

    implementando teste do metodo delete

--- src/test/java/br/com/restdemo/demo/service/artist/ArtistServiceTest.java
+++ src/test/java/br/com/restdemo/demo/service/artist/ArtistServiceTest.java
@@ -1,9 +1,9 @@
 package br.com.restdemo.demo.service.artist;
 
-import br.com.restdemo.demo.integration.artist.ArtistIntegration;
 import br.com.restdemo.demo.integration.artist.model.response.ArtistIntegrationResponse;
 import br.com.restdemo.demo.model.artist.ArtistEntity;
 import br.com.restdemo.demo.repository.artist.ArtistRepository;
+import br.com.restdemo.demo.service.artist.mapper.response.ArtistEntityIntegrationMapper;
 import br.com.restdemo.demo.service.artist.mapper.response.ArtistEntityResponseMapper;
 import br.com.restdemo.demo.service.artist.model.response.ArtistServiceResponse;
 import org.junit.jupiter.api.Test;
@@ -12,8 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,23 +27,8 @@ class ArtistServiceTest {
     @Mock
     ArtistRepository artistRepository;
 
-//    @Mock
-//    ArtistIntegration artistIntegration;
-
     @Test
-    void whenFindArtistReturnArtist() {
-
-//        ArtistIntegrationResponse artistIntegrationResponse = ArtistIntegrationResponse.builder()
-//                .name("Eminem")
-//                .id("13")
-//                .link("https://www.deezer.com/artist/13")
-//                .share("https://www.deezer.com/artist/13?utm_source=deezer&utm_content=artist-13&utm_term=0_1651495407&utm_medium=web")
-//                .picture("https://api.deezer.com/artist/13/image")
-//                .nbAlbum(54)
-//                .nbFan(15307427)
-//                .tracklist("https://api.deezer.com/artist/13/top?limit=50")
-//                .type("artist")
-//                .build();
+    void whenFindArtistByIdReturnOptionalOfArtistEntity() {
 
         ArtistEntity artistEntity = ArtistEntity.builder()
                 .name("Eminem")
@@ -55,31 +42,19 @@ class ArtistServiceTest {
                 .type("artist")
                 .build();
 
-        ArtistServiceResponse resultExpected = ArtistServiceResponse.builder()
-                .name("Eminem")
-                .id("13")
-                .link("https://www.deezer.com/artist/13")
-                .share("https://www.deezer.com/artist/13?utm_source=deezer&utm_content=artist-13&utm_term=0_1651495407&utm_medium=web")
-                .picture("https://api.deezer.com/artist/13/image")
-                .nbAlbum(54)
-                .nbFan(15307427)
-                .tracklist("https://api.deezer.com/artist/13/top?limit=50")
-                .type("artist")
-                .build();
+        Optional<ArtistEntity> expected = Optional.ofNullable(artistEntity);
 
-//        Mockito.when(artistIntegration.findArtist("13"))
-//                .thenReturn(artistIntegrationResponse);
-        Mockito.when(artistRepository.findById("13"))
-                .thenReturn(Optional.ofNullable(artistEntity));
+        Mockito.when(artistService.findArtistById("13"))
+                .thenReturn(expected);
 
-        ArtistServiceResponse artistServiceResponse = artistService.findArtistById("13")
-                .map(ArtistEntityResponseMapper::toArtistEntity).get();
-        assertEquals(resultExpected, artistServiceResponse);
-        //TODO mockserver
+        Optional<ArtistEntity> actual = artistService.findArtistById("13");
+        assertEquals(expected, actual);
+    } //TODO mockserver
+
+    @Test
+    void deleteArtist() {
+        artistRepository.deleteById("13");
+
+        Mockito.verify(this.artistRepository,Mockito.atMostOnce()).deleteById("13");
     }
-
-
-//    @Test
-//    void deleteArtist() {
-//    }
 }
