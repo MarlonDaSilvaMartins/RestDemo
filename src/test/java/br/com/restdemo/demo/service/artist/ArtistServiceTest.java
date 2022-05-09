8e20ec6e86e7df97b252b8f5e09c62e0a1cc148e
Marlon Martins <marlon673@hotmail.com>
2022-05-09 15:06:10 -0300

    implementando teste de delete

--- src/test/java/br/com/restdemo/demo/service/artist/ArtistServiceTest.java
+++ src/test/java/br/com/restdemo/demo/service/artist/ArtistServiceTest.java
@@ -1,13 +1,10 @@
 package br.com.restdemo.demo.service.artist;
 
 import br.com.restdemo.demo.configuration.exceptions.DataNotFoundException;
-import br.com.restdemo.demo.configuration.exceptions.ErrorObject;
-import br.com.restdemo.demo.configuration.exceptions.ErrorResponse;
 import br.com.restdemo.demo.integration.artist.ArtistIntegration;
 import br.com.restdemo.demo.integration.artist.model.response.ArtistIntegrationResponse;
 import br.com.restdemo.demo.model.artist.ArtistEntity;
 import br.com.restdemo.demo.repository.artist.ArtistRepository;
-import br.com.restdemo.demo.service.artist.mapper.response.ArtistEntityResponseMapper;
 import br.com.restdemo.demo.service.artist.model.response.ArtistServiceResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,16 +14,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 import static br.com.restdemo.demo.service.artist.ArtistServiceStub.*;
-import static br.com.restdemo.demo.service.artist.mapper.response.ArtistEntityIntegrationMapper.toIntegrationResponse;
-import static br.com.restdemo.demo.service.artist.mapper.response.ArtistEntityResponseMapper.toArtistEntity;
 import static org.junit.jupiter.api.Assertions.*;
 
-//@DataMongoTest
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = {ArtistService.class, ArtistRepository.class})
 class ArtistServiceTest {
@@ -80,14 +72,18 @@ class ArtistServiceTest {
 
         var actual = exception.getMessage();
 
-        assertTrue(actual.contains(expected));
+        assertEquals(actual, expected);
     }
 
     @Test
     void whenDeleteArtistReturnNothing() {
-        artistRepository.deleteById("13");
+        Mockito.doNothing().when(artistRepository).deleteById("13");
 
-        Mockito.verify(this.artistRepository,Mockito.atMostOnce()).deleteById("13");
+        artistService.deleteArtist("13");
+
+        var actual = Mockito.verify(this.artistRepository, Mockito.atMostOnce()).findById("13");
+
+        assertNull(actual);
     }//TODO mongo Embedded
 
     //TODO consumir outro endpoint para testar erro
